e42573fbc7754bbceec38976eba653466146ca67
SajeelKhan <sajeelraheel97@gmail.com>
2018-12-04 21:59:59 +0500

    Front End Complete
    Web Assignment Completed
    Front End Completed
    Sajeel

---
+++ AboutUs.php
@@ -0,0 +1,14 @@
+<?php include "./Partials/Header.php"; ?>
+
+<div class="AboutUs text-center" style="background-color: #3D9970" >
+
+    <h1 class="display-1 "> About Us</h1>
+
+    <ul class="list-group-flush "></ul>
+    <li class="list-group-item active display-4 text-left" ><b>Created By: </b></li>
+    <li class="list-group-item disabled "> <b>Name: </b> M. Sajeel Raheel <b>Roll No: </b> FA15-BSE-098</li>
+    <li class="list-group-item disabled "> <b>Name: </b> Adnan Fareed <b>Roll No: </b> FA15-BSE-008</li>
+
+</div>
+
+<?php include "./Partials/Footer.php";?>
